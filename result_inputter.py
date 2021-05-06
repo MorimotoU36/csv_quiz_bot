b7aa969c6bd9085ea108fa1b9da8be41b73b9854
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-05-06 16:16:51 +0900

    result_inputter.py 複数ファイル対応

--- result_inputter.py
+++ result_inputter.py
@@ -36,10 +36,12 @@ if(results == []):
     sys.exit()
 
 #問題csvのバックアップファイル作成
-quizfilename=""
+quiz_file_names=[]
 try:
-    quizfilename=ini['Filename']['QUIZFILE']
-    shutil.copyfile('csv/'+quizfilename,'csv/bkup/'+quizfilename+'.bkup')
+    quiz_file_names=json.loads(ini.get("Filename","QUIZ_FILE_NAME"))
+    for i in range(len(quiz_file_names)):
+        quizfilename=quiz_file_names[i]
+        shutil.copyfile('csv/'+quizfilename,'csv/bkup/'+quizfilename+'.bkup')
 except Exception as e:
     print("エラー：問題csv({0})のバックアップファイル作成時にエラーが発生しました".format(quizfilename),file=sys.stderr)
     print(e,file=sys.stderr)
@@ -58,9 +60,10 @@ except Exception as e:
 
 
 #問題csv読み込み
-df=""
+df=[]
 try:
-    df=pd.read_csv('csv/'+quizfilename)
+    for i in range(len(quiz_file_names)):
+        df.append(pd.read_csv('csv/'+quiz_file_names[i]))
 except Exception as e:
     print("エラー：問題csv({0})の読み込み時にエラーが発生しました".format(quizfilename),file=sys.stderr)
     print(e,file=sys.stderr)
@@ -70,30 +73,41 @@ except Exception as e:
 #結果データを解析
 try:
     for r in results:
+        file_id=r['file_id']
         quiz_id=r['quiz_id']
         ans=r['result']
 
-        #エラーハンドリング：問題番号が数字であるか？または有効な範囲内の数字であるかを確認
-        if(not quiz_id.isdecimal() or int(quiz_id) > df.shape[0]):
+        #エラーハンドリング：ファイル番号・問題番号が数字であるか？または有効な範囲内の数字であるかを確認
+        if(not file_id.isdecimal() or int(file_id) <= 0 or len(quiz_file_names) < int(file_id) ):
+            print('エラー：問題ファイル番号[{0}]はありません、飛ばします'.format(file_id),file=sys.stderr)
+            continue
+        elif(not quiz_id.isdecimal() or int(quiz_id) <= 0 or df[int(file_id)-1].shape[0] < int(quiz_id) ):
             print('エラー：問題番号[{0}]はありません、飛ばします'.format(quiz_id),file=sys.stderr)
             continue
         
-        #dfを問題番号でソート
-        df.sort_values('問題番号',inplace=True)
+        #指定したfile_idのdfを取り出す
+        df_i=df[int(file_id)-1]
+
+        #df_iを問題番号でソート
+        df_i.sort_values('問題番号',inplace=True)
         #quiz_idが何行目にあるかを調べる
-        idx=df.loc[df['問題番号'] == int(quiz_id)].index[0]
+        idx=df_i.loc[df_i['問題番号'] == int(quiz_id)].index[0]
 
         if(ans != "0"):
             #正解
-            df.at[idx,'正解数'] = str(int(df.at[idx,'正解数']) + 1)
+            df_i.at[idx,'正解数'] = str(int(df_i.at[idx,'正解数']) + 1)
         else:
             #不正解
-            df.at[idx,'不正解数'] = str(int(df.at[idx,'不正解数']) + 1)
+            df_i.at[idx,'不正解数'] = str(int(df_i.at[idx,'不正解数']) + 1)
         
-        print("問題["+quiz_id+"]:"+("正解" if ans != "0" else "不正解")+"+1")
+        #df_iを反映する
+        df[int(file_id)-1]=df_i
+
+        print("問題ファイル["+quiz_file_names[int(file_id)-1]+"] 問題["+quiz_id+"]:"+("正解" if ans != "0" else "不正解")+"+1")
     
     #反映した結果をcsvに更新する
-    df.to_csv('csv/'+quizfilename,index=False)
+    for i in range(len(quiz_file_names)):
+        df[i].to_csv('csv/'+quiz_file_names[i],index=False)
 
     print('{0}個の解答データを登録しました'.format(len(results)))
 except Exception as e:
