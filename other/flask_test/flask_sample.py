e5b3308fbed8b45ab36dd0a4909fcca6a808eb80
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-09-01 22:46:04 +0900

    ルーティングを追加してHello Worldを実装

--- other/flask_test/flask_sample.py
+++ other/flask_test/flask_sample.py
@@ -3,6 +3,10 @@ app = Flask(__name__, static_folder='.', static_url_path='')
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
- 
+
+@app.route('/hello')
+def hello():
+    return  "Hello! World!"
+
 app.run(port=8000, debug=True)
  
