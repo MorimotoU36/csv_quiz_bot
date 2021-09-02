441015a10a11d631672a3b61ae4129e7639376ba
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-09-02 22:50:46 +0900

    API用　CSVファイルリスト返す関数　APIで呼び出す様に改変

--- api/get_csv_filename_list.py
+++ api/get_csv_filename_list.py
@@ -1,44 +1,40 @@
 # -*- coding: utf-8 -*-
 from argparse import ArgumentParser
-import pandas as pd
 import configparser
 import sys
-import random
-import requests
-import time
 import json
 import os
 
+def getCsvFileNameList():
+    #カレントディレクトリからスクリプトのあるディレクトリへ移動
+    pwd_dir=os.getcwd()
+    pgm_dir=os.path.realpath(os.path.dirname(__file__))
+    os.chdir(pgm_dir)
 
-#カレントディレクトリからスクリプトのあるディレクトリへ移動
-pwd_dir=os.getcwd()
-pgm_dir=os.path.realpath(os.path.dirname(__file__))
-os.chdir(pgm_dir)
+    #設定ファイル読み込み
+    inifile="../config/quiz.ini"
+    ini=""
+    try:
+        ini = configparser.ConfigParser()
+        ini.read(inifile, 'UTF-8')
+    except Exception as e:
+        print("エラー：設定ファイル({0})が読み込めません".format(inifile))
+        print(e,file=sys.stderr)
+        os.chdir(pwd_dir)
+        sys.exit()
 
-#設定ファイル読み込み
-inifile="../config/quiz.ini"
-ini=""
-try:
-    ini = configparser.ConfigParser()
-    ini.read(inifile, 'UTF-8')
-except Exception as e:
-    print("エラー：設定ファイル({0})が読み込めません".format(inifile))
-    print(e,file=sys.stderr)
+    #リストと番号取得して返す
+    try:
+        csv_name_list=[]
+        file_names=json.loads(ini.get("Filename","QUIZ_FILE_NAME"))
+        for i,f in enumerate(file_names):
+            csv_name_list.append([i+1,f['csvname']])
+
+        return csv_name_list
+    except Exception as e:
+        print(e,file=sys.stderr)
+        os.chdir(pwd_dir)
+        sys.exit()
+
+    #元のディレクトリに戻る
     os.chdir(pwd_dir)
-    sys.exit()
-
-#リストと番号取得して返す
-try:
-    csv_name_list=[]
-    file_names=json.loads(ini.get("Filename","QUIZ_FILE_NAME"))
-    for i,f in enumerate(file_names):
-        csv_name_list.append([i+1,f['csvname']])
-
-    print(csv_name_list)
-except Exception as e:
-    print(e,file=sys.stderr)
-    os.chdir(pwd_dir)
-    sys.exit()
-
-#元のディレクトリに戻る
-os.chdir(pwd_dir)
