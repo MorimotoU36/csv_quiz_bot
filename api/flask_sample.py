2ac1564fb01b5521b9c26737525e18565d35604e
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-09-03 08:19:47 +0900

    Flask API用ファイル　とりあえずさっきの関数と組み合わせて　CSVのリスト返すAPIを作成

--- api/flask_sample.py
+++ api/flask_sample.py
@@ -1,12 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect
+import get_csv_filename_list
+
 app = Flask(__name__, static_folder='.', static_url_path='')
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
 
-@app.route('/hello')
-def hello():
-    return  "Hello! World!"
+@app.route('/getcsvlist')
+def getCsvList():
+    return str(get_csv_filename_list.getCsvFileNameList())
 
 app.run(port=8000, debug=True)
  
