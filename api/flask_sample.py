5f0d9471aeb21f876697677e69647cea2c85cde7
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-09-05 21:40:33 +0900

    Flask API用ファイル CORS設定完了、、厳密にはもうちょっとセキュリティ設定必要

--- api/flask_sample.py
+++ api/flask_sample.py
@@ -1,7 +1,9 @@
 from flask import Flask, redirect
+from flask_cors import CORS 
 import get_csv_filename_list
 
 app = Flask(__name__, static_folder='.', static_url_path='')
+CORS(app)
 @app.route('/')
 def index():
     return app.send_static_file('index.html')
