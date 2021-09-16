1e104b3ca6e82526dc83c2cae616e7da09599fa4
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-09-17 00:26:22 +0900

    正解データ登録API追加

--- api/flask_sample.py
+++ api/flask_sample.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, request
 from flask_cors import CORS 
 import get_csv_filename_list
 import get_question
+import answer
 
 app = Flask(__name__, static_folder='.', static_url_path='')
 CORS(app)
@@ -22,5 +23,14 @@ def getQuestion():
     print(get_question.getQuestion(int(file_num)-1,int(quiz_num)-1))
     return get_question.getQuestion(int(file_num)-1,int(quiz_num)-1)
 
+@app.route('/correct', methods=['GET', 'POST'])
+def correct():
+    # 送信データを取得、バイト文字列なのでデコードする
+    post_data=request.get_data().decode()
+    file_num,quiz_num=post_data.split('-')
+    print(file_num,quiz_num)
+    print(get_question.getQuestion(int(file_num)-1,int(quiz_num)-1))
+    return answer.correct(int(file_num)-1,int(quiz_num))
+
+
 app.run(port=8000, debug=True)
- 
