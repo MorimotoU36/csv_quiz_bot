8d2f65ef42a84cd487f40e2a2f621600af8c7bb1
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-10-05 22:36:51 +0900

    問題編集API 　英語問題の更新機能実装

--- aws_api_gateway/edit_quiz.py
+++ aws_api_gateway/edit_quiz.py
@@ -11,9 +11,15 @@ dynamodb = boto3.resource('dynamodb')
 
 def lambda_handler(event, context):
     # 値取得
+    file_num=event['file_num']
+    
+    #英語版なら分岐
+    if(file_num=='E'):
+        return update_english_question(event)
+    
+    # 値取得
     file=event['file']
     number=event['number']
-    file_num=event['file_num']
     sentense=event['sentense']
     answer=event['answer']
     category=event['category']
@@ -61,6 +67,55 @@ def lambda_handler(event, context):
     except Exception as e:
         status_code = "500"
         message = 'Error. Pushing Index['+file+'-'+number+'] Data Failed. :'+str(e)
+
+    return {
+        'statusCode': status_code,
+        'message': message
+    }
+
+def update_english_question(req):
+    # 値取得
+    number=req['number']
+    sentense=req['sentense']
+    answer=req['answer']
+    category=req['category']
+    img_file_name=req['img_file_name']
+    
+    #DynamoDBテーブルを選択
+    quiz_table = dynamodb.Table('english_speaking')
+
+    status_code=""
+    message=""
+    try:
+        # テーブルへのUpdate処理実行
+        quiz_table.update_item(
+                Key={'quiz_num': int(number)},
+                UpdateExpression="set #sentense = :quiz_sentense, #answer = :answer, #category = :category, #img_file_name = :img_file_name",
+                ExpressionAttributeNames={
+                    '#sentense': 'quiz_sentense',
+                    '#answer': 'answer',
+                    '#category': 'category',
+                    '#img_file_name': 'img_file_name'
+                },
+                ExpressionAttributeValues={
+                    ':quiz_sentense': sentense,
+                    ':answer': answer,
+                    ':category': category,
+                    ':img_file_name': img_file_name
+                },
+                ReturnValues="UPDATED_NEW"
+            )
+        status_code = "200"
+        message = 'Edited! [和文英訳-'+number+']'+sentense+','+answer+',0,0,'+category+','+img_file_name
+    except ClientError as ce:
+        status_code = "500"
+        message = 'Error. Pushing Index[和文英訳-'+number+'] Data Failed. :'+str(ce)
+    except TypeError as e:
+        status_code = "500"
+        message = 'Error. Pushing Index[和文英訳-'+number+'] Data Failed. :'+str(e)
+    except Exception as e:
+        status_code = "500"
+        message = 'Error. Pushing Index[和文英訳-'+number+'] Data Failed. :'+str(e)
 
     return {
         'statusCode': status_code,
