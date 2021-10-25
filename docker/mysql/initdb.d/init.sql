3f8677a6109f88a49cda19f62b615222ef81e34f
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-10-25 23:57:54 +0900

    他のテーブルのDDLも作成

--- docker/mysql/initdb.d/init.sql
+++ docker/mysql/initdb.d/init.sql
@@ -13,3 +13,39 @@ CREATE TABLE IF NOT EXISTS aws_quiz
   img_file VARCHAR(128)
 ) DEFAULT CHARACTER
   SET=utf8;
+
+CREATE TABLE IF NOT EXISTS applied
+(
+  quiz_num INT NOT NULL PRIMARY KEY,
+  quiz_sentense VARCHAR(256) NOT NULL,
+  answer VARCHAR(256) NOT NULL,
+  clear_count INT DEFAULT 0,
+  fail_count INT DEFAULT 0,
+  category VARCHAR(256),
+  img_file VARCHAR(128)
+) DEFAULT CHARACTER
+  SET=utf8;
+
+CREATE TABLE IF NOT EXISTS lpic
+(
+  quiz_num INT NOT NULL PRIMARY KEY,
+  quiz_sentense VARCHAR(256) NOT NULL,
+  answer VARCHAR(256) NOT NULL,
+  clear_count INT DEFAULT 0,
+  fail_count INT DEFAULT 0,
+  category VARCHAR(256),
+  img_file VARCHAR(128)
+) DEFAULT CHARACTER
+  SET=utf8;
+
+CREATE TABLE IF NOT EXISTS english_speaking
+(
+  quiz_num INT NOT NULL PRIMARY KEY,
+  quiz_sentense VARCHAR(256) NOT NULL,
+  answer VARCHAR(256) NOT NULL,
+  clear_count INT DEFAULT 0,
+  fail_count INT DEFAULT 0,
+  category VARCHAR(256),
+  img_file VARCHAR(128)
+) DEFAULT CHARACTER
+  SET=utf8;
