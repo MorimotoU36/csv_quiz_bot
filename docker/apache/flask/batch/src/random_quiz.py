5ccab0b77bcdc78ef97cb6ae599199fa966276be
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-11-03 22:38:34 +0900

    random_quiz  とりあえず形にはした

--- docker/apache/flask/batch/src/random_quiz.py
+++ docker/apache/flask/batch/src/random_quiz.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import random
 import traceback
 import pymysql
 import pymysql.cursors
@@ -13,13 +14,43 @@ def random_quiz(file_num=-1,image=True,rate=100.0):
 
     # 設定ファイルを呼び出してファイル番号からテーブル名を取得
     # (変なファイル番号の時はランダムに選ぶ)
-
+    table_list = get_table_list()
+    if(file_num < 0 or len(table_list) <= file_num):
+        file_num = random.randint(0,len(table_list)-1)
+    table = table_list[file_num]['name']
+    nickname = table_list[file_num]['nickname']
+        
     # MySQL への接続を確立する
-
+    try:
+        conn = get_connection()
+    except Exception as e:
+        print('Error: DB接続時にエラーが発生しました')
+        print(traceback.format_exc())
+        sys.exit()
+    
     # テーブル名からSQLを作成して投げる
-    # 指定したテーブルの件数を調べる
-    # 問題番号をランダムで選ぶ
+    with conn.cursor() as cursor:
+        # 指定したテーブルの件数を調べる
+        sql = "SELECT count(*) FROM {0}".format(table)
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        count = results[0]['count(*)']
 
-    # SQL作って投げて問題を取得する
+        # 問題番号をランダムで選ぶ
+        quiz_num = random.randint(1,count)
+
+        # SQL作成して問題を取得する
+        sql = "SELECT quiz_num, quiz_sentense, answer, clear_count, fail_count, category, img_file FROM {0} WHERE quiz_num = {1}".format(table,quiz_num)
+        print(sql)
+        cursor.execute(sql)
+
+        # MySQLから帰ってきた結果を受け取る
+        # Select結果を取り出す
+        results = cursor.fetchall()
 
     # 結果をJSONに変形して返す
+    return results[0]
+
+if __name__=="__main__":
+    res = random_quiz(file_num=-1)
+    print(res)
