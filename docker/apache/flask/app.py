3770917b9d089da05895fe924488ab3ebeddee1a
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-11-03 18:39:32 +0900

    select_quiz とりあえずFlaskでAPI立てて外部から疎通可能に　Dockerのコンテナ間ネットワーク設定という方法を新たに知った。

--- docker/apache/flask/app.py
+++ docker/apache/flask/app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+# -*- coding: utf-8 -*-
+import traceback
+from batch.src.select_quiz import select_quiz
+
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/select', methods=["POST"])
+def select():
+    try:
+        # リクエストから値を読み取る
+        req = request.json
+        file_num = int(req.get("file_num"))
+        quiz_num = int(req.get("quiz_num"))
+        image_flag = bool(req.get("image"))
+
+        # MySQLに問題を取得しにいく
+        result = select_quiz(file_num,quiz_num,image_flag)
+
+        # 取得結果を返す
+        return result
+    except Exception as e:
+        return traceback.format_exc()
+        return {
+            "error" : traceback.format_exc()
+        }
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=4999, debug=True)
