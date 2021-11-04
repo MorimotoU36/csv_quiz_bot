f26d15f41b76199a164d1f1b0bef06295b8a09c7
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-11-04 21:25:42 +0900

    random_quiz  APIに反映

--- docker/apache/flask/app.py
+++ docker/apache/flask/app.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import traceback
 from batch.src.select_quiz import select_quiz
+from batch.src.random_quiz import random_quiz
 
 from flask import Flask, request
 app = Flask(__name__)
@@ -40,6 +41,36 @@ def select():
             "error" : traceback.format_exc()
         }
 
+@app.route('/random', methods=["POST"])
+def random():
+    """ランダム問題取得API
+    Args: JSON
+    {
+        "file_num": ファイル番号(オプション),
+        "image": 画像取得フラグ(オプション),
+        "rate": 正解率(オプション)
+    }
+
+    Returns:
+        result(JSON): 取得した問題またはエラーログ
+    """
+    try:
+        # リクエストから値を読み取る。ない場合はデフォルト値
+        req = request.json
+        file_num = int(req.get("file_num",-1))
+        image_flag = bool(req.get("image",True))
+        rate = float(req.get("rate",100))
+
+        # MySQLに問題を取得しにいく
+        result = random_quiz(file_num=file_num,image=image_flag,rate=rate)
+
+        # 取得結果を返す
+        return result
+    except Exception as e:
+        return traceback.format_exc()
+        return {
+            "error" : traceback.format_exc()
+        }
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=4999, debug=True)
