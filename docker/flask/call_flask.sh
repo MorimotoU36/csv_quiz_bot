c54227ed59c756dac86c5a1419a979e3b0d22f1c
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2021-11-28 00:15:04 +0900

    flask起動シェル追加

--- docker/flask/call_flask.sh
+++ docker/flask/call_flask.sh
@@ -4,6 +4,8 @@ PWD_DIR=${PWD}
 APP_DIR=`dirname $0`
 
 cd ${APP_DIR}
+echo "-----------------------------" >> nohup.out
+echo `date "+%Y/%m/%d %H:%M:%S"` >> nohup.out
 nohup python3 app.py &
 
 cd ${PWD_DIR}
