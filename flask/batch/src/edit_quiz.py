afdb6c0d6a58d3f072b9673161b99be33136254c
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-01-24 22:34:01 +0900

    edit_quizに指定した問題のチェック状態を変更する関数追加

--- flask/batch/src/edit_quiz.py
+++ flask/batch/src/edit_quiz.py
@@ -192,24 +192,124 @@ def edit_category_of_question(data):
 
 
 
+def edit_checked_of_question(data):
+    """問題のチェックを変更する関数
+
+    Args:
+        data ([JSON]): JSONの配列
+        要素は
+        {
+            "file_num" : ファイル番号(int)
+            "quiz_num" : 問題番号(int)
+        }
+    """
+
+    # テーブルリスト取得
+    table_list = get_table_list()
+
+    # MySQL への接続を確立する
+    try:
+        conn = get_connection()
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "message": 'Error: DB接続時にエラーが発生しました',
+            "traceback": traceback.format_exc()
+        }
+
+    try:
+        # data内のクエリを１個１個見ていく
+        for data_i in data:
+            # ファイル番号を取得
+            file_num = data_i['file_num']
+            # 問題番号を取得
+            quiz_num = data_i['quiz_num']
+
+            # テーブル名取得
+            table = table_list[file_num]['name']
+            nickname = table_list[file_num]['nickname']
+
+            with conn.cursor() as cursor:
+                # まずは問題を取得
+                sql = "SELECT checked FROM {0} WHERE quiz_num = {1}".format(table,quiz_num)
+                cursor.execute(sql)
+
+                # MySQLから帰ってきた結果を受け取る
+                # Select結果を取り出す
+                results = cursor.fetchall()
+
+                # チェック状態を取得
+                checked = int(results[0]['checked'])
+                print(checked)
+
+                # チェック状態を修正する
+                if(checked == 0):
+                    # チェックなしならチェックありにする
+                    sql = "UPDATE {0} SET checked = {1} WHERE quiz_num = {2} ".format(table,1,quiz_num)
+                    cursor.execute(sql)
+                else:
+                    # チェックありならチェックなしにする
+                    sql = "UPDATE {0} SET checked = {1} WHERE quiz_num = {2} ".format(table,0,quiz_num)
+                    cursor.execute(sql)
+
+        #全て成功したらコミット
+        conn.commit()
+        conn.close()
+
+        return {
+            "statusCode": 200,
+            "message": "All OK."
+        }
+
+    # DB操作失敗時はロールバック
+    except Exception as e:
+        message = 'Error: DB接続時にエラーが発生しました '
+        try:
+            conn.rollback()
+        except:
+            message += '( ロールバックにも失敗しました )'
+        finally:
+            return {
+                "statusCode": 500,
+                "message": message,
+                "traceback": traceback.format_exc()
+            }
+
+
+
 if __name__=="__main__":
 #    res = edit_quiz(2,99,"ques1","ans2","cat3","img4")
+    # data = [
+    #     {
+    #         "file_num" : 2,
+    #         "quiz_num" : 93,
+    #         "category" : "テスト"
+    #     },
+    #     {
+    #         "file_num" : 2,
+    #         "quiz_num" : 94,
+    #         "category" : "テスト"
+    #     },
+    #     {
+    #         "file_num" : 2,
+    #         "quiz_num" : 95,
+    #         "category" : "テスト"
+    #     }
+    # ]
+    # res = edit_category_of_question(data)
     data = [
         {
             "file_num" : 2,
-            "quiz_num" : 93,
-            "category" : "テスト"
+            "quiz_num" : 93
         },
         {
             "file_num" : 2,
-            "quiz_num" : 94,
-            "category" : "テスト"
+            "quiz_num" : 94
         },
         {
             "file_num" : 2,
-            "quiz_num" : 95,
-            "category" : "テスト"
+            "quiz_num" : 95
         }
     ]
-    res = edit_category_of_question(data)
+    res = edit_checked_of_question(data)
     print(res)
