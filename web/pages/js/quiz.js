805fbf35498e4d272e6aeca67c857b8201ae7e65
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-05-30 10:55:23 +0900

    最小正解数取得　削除済問題を取らないように変更　あとこれまでのロジックも少し変更

--- web/pages/js/quiz.js
+++ web/pages/js/quiz.js
@@ -232,7 +232,7 @@ function random_select_question(server){
 
             // 削除フラグありならメッセージ表示して終了
             if(response.deleted == 1){
-                set_error_message("エラー : 問題["+data.quiz_num+"] は削除済です");
+                set_error_message("エラー : 問題["+response.quiz_num+"] は削除済です");
                 return false
             }
 
@@ -304,7 +304,7 @@ function worst_rate_question(server){
 
             // 削除フラグありならメッセージ表示して終了
             if(response.deleted == 1){
-                set_error_message("エラー : 問題["+data.quiz_num+"] は削除済です");
+                set_error_message("エラー : 問題["+response.quiz_num+"] は削除済です");
                 return false
             }
 
@@ -340,7 +340,7 @@ function worst_rate_question(server){
     })
 }
 
-//正解率最低の問題を出題する
+//正解数最低の問題を出題する
 function minimum_clear_question(server){
     //メッセージをクリア
     clear_all_message();
@@ -373,6 +373,13 @@ function minimum_clear_question(server){
             let question = document.getElementById("question")
             let answer = document.getElementById("answer")
             let response = resp.response
+
+            // 削除フラグありならメッセージ表示して終了
+            if(response.deleted == 1){
+                set_error_message("エラー : 問題["+response.quiz_num+"] は削除済です");
+                return false
+            }
+
             sentense = response.quiz_sentense === undefined ? "" : "["+response.quiz_num+"]"+response.quiz_sentense
             quiz_answer =  response.answer === undefined ? "" : "["+response.quiz_num+"]"+response.answer
             image_file =  response.img_file === undefined ? "" : response.img_file
