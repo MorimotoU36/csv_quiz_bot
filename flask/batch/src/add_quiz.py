7aaaad06ae65521f0dd17d38b19a0fe8f4cdbf5d
Wataru Tsukagoshi <motohari.xanadu@gmail.com>
2022-05-30 11:43:05 +0900

    問題追加API  追加時のSQL、削除済問題あったときはそこの番号に優先して入れる処理に変更

--- flask/batch/src/add_quiz.py
+++ flask/batch/src/add_quiz.py
@@ -10,7 +10,7 @@ from dbconfig import get_connection
 from ini import get_table_list
 
 def add_quiz(file_num,input_data):
-    """ファイル番号、問題番号、イメージ取得フラグから問題を取得する関数
+    """問題を追加する関数
 
     Args:
         file_num (int): ファイル番号
@@ -46,15 +46,6 @@ def add_quiz(file_num,input_data):
         }
 
     try:
-        # データ全件数を確認
-        count = 0
-        with conn.cursor() as cursor:
-            # 指定したテーブルの件数を調べる
-            sql = "SELECT count(*) FROM {0}".format(table)
-            cursor.execute(sql)
-            results = cursor.fetchall()
-            count = results[0]['count(*)']
-
         # 返るデータ
         result = []
 
@@ -69,18 +60,44 @@ def add_quiz(file_num,input_data):
             img_file = data_i[3]
 
             # データのidを作成
-            count += 1
-
-            # テーブル名と問題番号からSQLを作成して投げる
-            # (問題番号が範囲外なら終了)
-            # SQLを実行する
+            new_quiz_id = -1
             with conn.cursor() as cursor:
-                # データを挿入する
-                sql = "INSERT INTO {0} VALUES({1},'{2}','{3}',0,0,'{4}','{5}',0)".format(table,count,question,answer,category,img_file)
-                print(sql)
+                # 削除済問題がある場合はその番号に入れる
+                sql = "SELECT quiz_num FROM {0} WHERE deleted = 1 ORDER BY quiz_num LIMIT 1 ".format(table)
                 cursor.execute(sql)
+                sql_results = cursor.fetchall()
 
-                result.append("Added!! [{0}-{1}]:{2},{3}".format(nickname,str(count),question,answer))
+            if(len(sql_results) > 0):
+                new_quiz_id = sql_results[0]['quiz_num']
+                # 削除済問題のところにデータを更新する形でいれる
+                with conn.cursor() as cursor:
+                    sql = "UPDATE {0} SET quiz_sentense = '{1}', answer = '{2}', clear_count = 0, fail_count = 0, category = '{3}', img_file = '{4}', checked = 0, deleted = 0 WHERE quiz_num = {5} ".format(table,question,answer,category,img_file,new_quiz_id)
+                    print(sql)
+                    cursor.execute(sql)
+
+                    result.append("Added!! [{0}-{1}]:{2},{3}".format(nickname,str(new_quiz_id),question,answer))
+
+            else:
+                # 削除済問題がない場合
+                # データ全件数から番号を決定する
+                with conn.cursor() as cursor:
+                    # 指定したテーブルの件数を調べる
+                    sql = "SELECT count(*) FROM {0}".format(table)
+                    cursor.execute(sql)
+                    results = cursor.fetchall()
+                    new_quiz_id = results[0]['count(*)']
+                    new_quiz_id += 1
+
+                # テーブル名と問題番号からSQLを作成して投げる
+                # (問題番号が範囲外なら終了)
+                # SQLを実行する
+                with conn.cursor() as cursor:
+                    # データを挿入する
+                    sql = "INSERT INTO {0} VALUES({1},'{2}','{3}',0,0,'{4}','{5}',0,0)".format(table,new_quiz_id,question,answer,category,img_file)
+                    print(sql)
+                    cursor.execute(sql)
+
+                    result.append("Added!! [{0}-{1}]:{2},{3}".format(nickname,str(new_quiz_id),question,answer))
 
         else:
             #全て成功したらコミット
